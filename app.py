b081aed19e4846e71411089e9a8badf1887e7a0c
Abhay Kumar <86096153+abhaykr489@users.noreply.github.com>
2023-10-22 03:10:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,55 +1,21 @@
 import os
 import uuid
 import time
-
-from numpy.random.mtrand import random
 from werkzeug.utils import secure_filename
 from flask import Flask, render_template, request, jsonify, send_from_directory
 import cv2
 import re
 import pytesseract
 import numpy as np
-import base64
-import random
 from PIL import Image, ImageChops
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
 app.config['IMAGES_FOLDER'] = 'images'
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
-
-def detect_text_alterations(image_path):
-    # Perform OCR to extract text from the image
-    extracted_text = pytesseract.image_to_string(Image.open(image_path))
-
-    # Implement logic to detect alterations in the extracted text
-    # For example, check for spelling mistakes, inconsistent fonts, or formatting changes
-    # If inconsistencies are found, consider it as text forgery
-    if has_inconsistencies(extracted_text):
-        print("Text forgery detected")
-        return True
-    else:
-        print("No text forgery detected")
-        return False
-
-def detect_visual_alterations(image_path):
-    image = cv2.imread(image_path, 0)  # Read the image as grayscale
-
-    # Apply Canny edge detection
-    edges = cv2.Canny(image, 100, 200)
-
-    # Implement logic to detect alterations in the edges image
-    # If unexpected edges or shapes are found, consider it as visual forgery
-    if has_unexpected_edges(edges):
-        print("Visual forgery detected")
-        return True
-    else:
-        print("No visual forgery detected")
-        return False
 
 def has_unexpected_edges(edges):
     edge_threshold = 10
@@ -89,12 +55,10 @@ def detect_scribbling_or_overwriting(text):
         print("No scribbling or overwriting forgery detected")
         return False  # No scribbling or overwriting forgery detected
 
-def detect_whitener(image_path):
+def detect_whitener_forgery(image_path):
     # Load the image in color mode
     color_image = cv2.imread(image_path, cv2.IMREAD_COLOR)
-
     if color_image is None:
-        print("Error: Failed to load the image.")
         return False
 
     # Convert the color image to grayscale
@@ -114,35 +78,118 @@ def detect_whitener(image_path):
     whitener_percentage = whitener_pixel_count / total_pixels
 
     # Check if the percentage of whitener pixels exceeds the threshold
+    if whitener_percentage > whitener_percentage_threshold:
+        print("whitener forgery detected")
+    else:
+        print("No whitner forgery")
     return whitener_percentage > whitener_percentage_threshold
 
+#function to detect digital forgery
 def detect_digital_forgery(image_path):
     original_image = Image.open(image_path)
-
+    # Convert the image to RGB mode
+    original_image = original_image.convert('RGB')
     # Save the original image in a temporary file
-    original_image.save("original_temp.jpg", quality=90)
-
+    original_image.save("original_temp2.jpg", quality=90)
     # Open the saved original image
-    original_temp = Image.open("original_temp.jpg")
-
+    original_temp = Image.open("original_temp2.jpg")
     # Calculate Error Level Analysis (ELA)
     ela_image = ImageChops.difference(original_temp, original_image)
     extrema = ela_image.getextrema()
     max_diff = max([ex[1] for ex in extrema])
-
     # Set a threshold for forgery detection (adjust based on testing)
     threshold = 40
 
     # Check if the maximum difference exceeds the threshold
+    if max_diff > threshold:
+        print("digital forgery detected")
+    else:
+        print("No digital forgery")
     return max_diff > threshold
+
+#function to detect text alteration
+def preprocess_image(image_path):
+    # Read the image and apply preprocessing techniques
+    image = cv2.imread(image_path)
+
+    # Denoise the image
+    denoised_image = cv2.fastNlMeansDenoisingColored(image, None, 10, 10, 7, 15)
+
+    # Enhance contrast
+    lab = cv2.cvtColor(denoised_image, cv2.COLOR_BGR2LAB)
+    l, a, b = cv2.split(lab)
+    clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
+    cl = clahe.apply(l)
+    limg = cv2.merge((cl, a, b))
+    contrast_enhanced_image = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
+
+    # Convert the enhanced image to grayscale
+    gray_image = cv2.cvtColor(contrast_enhanced_image, cv2.COLOR_BGR2GRAY)
+
+    # Thresholding to create a binary image
+    _, binary_image = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    return binary_image
+
+#function to detect data manipulation forgery
+def detect_data_manipulation_forgery(image_path):
+        # Perform OCR on the uploaded image
+        preprocessed_image = preprocess_image(image_path)
+        extracted_text = pytesseract.image_to_string(preprocessed_image)
+
+        # Define regular expressions for critical data fields
+        date_pattern = r'\b\d{2}/\d{2}/\d{4}\b'  # Pattern for date (dd/mm/yyyy)
+        name_pattern = r'\b[A-Z][a-z]+\s[A-Z][a-z]+\b'  # Pattern for full name (First Last)
+        amount_pattern = r'\$\d+\.\d{2}\b'  # Pattern for currency amount ($XXX.XX)
+        invoice_pattern = r'INV-\d{4}'  # Pattern for invoice number (INV-XXXX)
+
+        # Check if any of the critical data patterns are found in the extracted text
+        date_match = re.search(date_pattern, extracted_text)
+        name_match = re.search(name_pattern, extracted_text)
+        amount_match = re.search(amount_pattern, extracted_text)
+        invoice_match = re.search(invoice_pattern, extracted_text)
+
+        # If any critical data pattern is not found, consider it a data manipulation forgery
+        if not (date_match and name_match and amount_match and invoice_match):
+            print("Data manipulation forgery detected")
+            return True  # Data manipulation forgery detected
+        else:
+            print("No data manipulation forgery detected")
+            return False  # No data manipulation forgery detected
+
+def detect_text_alteration_forgery(image_path):
+    uniformity_threshold = 0.9
+
+    preprocessed_image = preprocess_image(image_path)
+
+    # Perform OCR on the preprocessed image
+    extracted_text = pytesseract.image_to_string(preprocessed_image)
+
+    # Calculate uniformity of characters in the extracted text
+    total_characters = len(extracted_text)
+    unique_characters = len(set(extracted_text))
+
+    if total_characters > 0:
+        uniformity = unique_characters / total_characters
+
+        # Compare uniformity with the threshold
+        if uniformity < uniformity_threshold:
+            print("Text alteration forgery detected")
+            return True
+        else:
+            print("No text alteration forgery detected")
+            return False
+    else:
+        print("No text detected or empty text. Skipping text alteration forgery check.")
+        return False
 
 # Function to detect and mark forgery
 def detect_and_mark_forgery(original_path):
     scribbling_or_overwriting = detect_scribbling_or_overwriting(original_path)
     digital_forgery = detect_digital_forgery(original_path)
-    whitener_detected = detect_whitener(original_path)
-    text_forgery = detect_text_alterations(original_path)
-    visual_forgery = detect_visual_alterations(original_path)
+    whitener_detected = detect_whitener_forgery(original_path)
+    text_alteration_forgery = detect_text_alteration_forgery(original_path)
+    data_manipulation = detect_data_manipulation_forgery(original_path)
 
     unique_filename = None  # Default value for unique_filename
     forgery_types = []
@@ -152,36 +199,32 @@ def detect_and_mark_forgery(original_path):
     detected_count = 0
 
     if scribbling_or_overwriting:
-        forgery_types.append("Overwriting-Forgery")
+        forgery_types.append("Overwriting_Forgery")
         detected_count += 1
 
     if digital_forgery:
-        forgery_types.append("Digital-Forgery")
+        forgery_types.append("Digital_Forgery")
         detected_count += 1
 
     if whitener_detected:
-        forgery_types.append("Whitener-Forgery")
+        forgery_types.append("Whitener_Forgery")
         detected_count += 1
 
-    if text_forgery:
-        forgery_types.append("Text-Forgery")
-        detected_count += 1
+    if text_alteration_forgery:
+         forgery_types.append("Text_alteration_Forgery")
+         detected_count += 1
 
-    if visual_forgery:
-        forgery_types.append("Visual-Forgery")
-        detected_count += 1
+    if data_manipulation:
+         forgery_types.append("data_manipulation_Forgery")
+         detected_count +=1
 
     # Calculate the percentage of forgery detected
     forgery_percentage = (detected_count / total_checks) * 100
-    accuracy = random.uniform(70, 100)
 
-    # Determine the forgery type based on detected alterations
-    forgery_type = " ".join(forgery_types)
+    forgery_type = ", ".join(forgery_types)
 
     # Check if any forgery is detected
     if detected_count > 0:
-        print(f"Forgery detected ({forgery_percentage:.2f}%): {forgery_type}")
-
         # Implement forgery marking logic here (e.g., draw colored rectangles or annotations)
         image = cv2.imread(original_path)
         forgery_area = (500, 500, 500, 500)  # Example forgery area: (x, y, width, height)
@@ -190,7 +233,6 @@ def detect_and_mark_forgery(original_path):
 
         # Generate a unique filename using a timestamp and a UUID
         unique_filename = str(int(time.time())) + '_' + str(uuid.uuid4()) + '.jpg'
-        print("Unique Filename:", unique_filename)  # Debug print
 
         # Convert image to RGB mode before saving as JPEG
         image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -198,14 +240,13 @@ def detect_and_mark_forgery(original_path):
 
         # Save the marked image to the IMAGES_FOLDER
         marked_image_path = os.path.join(app.config['IMAGES_FOLDER'], unique_filename)
-        pil_image.save(marked_image_path, format='PNG')  # Save the marked image as PNG
+        pil_image.save(marked_image_path)  # Save the marked image as PNG
 
-        return marked_image_path, forgery_percentage, forgery_type, accuracy
-
+        return marked_image_path, forgery_percentage, forgery_type
+    else:
     # No forgery detected
-    print("No forgery detected")
-    return original_path, 0, None
-
+     print("No forgery detected")
+     return original_path, 0, None
 
 # Route for file upload
 @app.route('/upload', methods=['POST'])
@@ -224,19 +265,15 @@ def upload():
             file.save(uploaded_image_path)
 
             # Detect forgery and get the marked image path and forgery type
-            marked_image_path, forgery_percentage, forgery_type, accuracy = detect_and_mark_forgery(uploaded_image_path)
+            marked_image_path, forgery_percentage, forgery_type = detect_and_mark_forgery(uploaded_image_path)
 
             if forgery_type:
-                # Format forgery type for JavaScript
-                formatted_forgery_type = forgery_type.lower().replace(' ', '-') + '-'
-
                 # Prepare the response object with forgery type and percentage
                 response = {
                     'message': 'Forgery detected successfully',
                     'forgery_image_path': marked_image_path,
-                    'forgery_type': formatted_forgery_type,
+                    'forgery_type': forgery_type,
                     'forgery_percentage': forgery_percentage,
-                    'accuracy': accuracy
                 }
             else:
                 # No forgery detected
